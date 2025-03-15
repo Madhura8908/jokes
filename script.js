c9220319ad49854d43ffc6e0d3a71d5d6f5c53c9
madhura <tambekarmadhura@gmail.com>
2025-03-15 13:10:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+let count =0;
+
+async function getjoke(){
+    document.getElementById('joke').innerText= '';
+    document.getElementById('setup').innerText= '';
+    document.getElementById('delivery').innerText= '';
+    let select=document.getElementById('select').value
+    let response=await fetch(`https://v2.jokeapi.dev/joke/${select}`)
+    let jokes=await response.json();
+   
+    if(jokes.type=='single'){
+        document.getElementById('joke').innerText=jokes.joke;
+    }else{
+        document.getElementById('setup').innerText=jokes.setup;
+        document.getElementById('delivery').innerText=jokes.delivery;
+    }
+    count++;
+    document.getElementById('count').innerText=`You have read ${count} pieces of jokes today!`
+    console.log(jokes)
+}
